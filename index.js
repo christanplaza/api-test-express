47ae0e703a728b94922d33decbb6b5c2c57807bf
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2023-04-19 03:35:29 +0800

    added CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 8000;
 
 app.listen(PORT, () => console.log(`Hosted at: ${PORT}`))
 
 app.use(express.json());
+app.use(cors());
 
 app.post('/users/:id', (req, res) => {
     const { id } = req.params;
